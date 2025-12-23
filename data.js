904a868b55ee6a0463f940db4716032d84b55514
YCDStudio <ycd.studio.ops@gmail.com>
2025-12-23 23:33:20 +0300

    Initial Update

---
+++ data.js
@@ -0,0 +1,137 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-211-primary-bathroom-1",
+      "name": "211-Primary Bathroom 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9487914907823685,
+          "pitch": 0.041307802064380894,
+          "rotation": 0,
+          "target": "2-100-bathroom"
+        },
+        {
+          "yaw": 0.8963758660680803,
+          "pitch": 0.014792326850132653,
+          "rotation": 0,
+          "target": "1-211-primary-bathroom-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-211-primary-bathroom-2",
+      "name": "211-Primary Bathroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9049667721012673,
+          "pitch": 0.06985396958317125,
+          "rotation": 0,
+          "target": "2-100-bathroom"
+        },
+        {
+          "yaw": 2.7597123369459258,
+          "pitch": 0.09120334028574106,
+          "rotation": 0,
+          "target": "0-211-primary-bathroom-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-100-bathroom",
+      "name": "100-Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7551636688753884,
+          "pitch": 0.0687680176013572,
+          "rotation": 0,
+          "target": "0-211-primary-bathroom-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "55 River Drive",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
